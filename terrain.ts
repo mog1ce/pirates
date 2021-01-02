ed1fd8f4f57cbaf82e48e9d2d588fb7fcf6c9a4e
Damien Hinsinger <damien@vegz.fr>
2021-01-02 11:48:01 +0100

    assets

---
+++ terrain.ts
@@ -0,0 +1,20 @@
+namespace sprites.terrain {
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._image
+    export const Terrain__32x32__a9 = image.ofBuffer(hex``);
+}
